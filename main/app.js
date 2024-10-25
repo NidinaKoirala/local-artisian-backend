482d5866a66128806c6394b13028bf50a14f3374
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-10-25 16:08:31 +0545

    modular routes for specific task

--- main/app.js
+++ main/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import session from "express-session";
-import db from "../prisma/database.js";
 import passport from "../passport/passportConfig.js";
-import bcrypt from "bcryptjs";
 import authorize from "./rbac.js";
+import authRoute from "./routes/authRoute.js";
+import adminRoute from "./routes/adminRoute.js";
 const PORT = 5173;
 
 const app = express();
@@ -13,50 +13,10 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use("/auth", authRoute);
+app.use("/admin", adminRoute);
 
 app.get("/", (req, res) => res.render("index", { user: req.user, req: req }));
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
-
 app.get("/books", authorize(['see_item', 'chat']), (req,res)=> res.json({name:'harry potter'}));
-
-app.post("/log-out", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
-
-const insertUserStmt = db.prepare(
-  "INSERT INTO User (username,email, password) VALUES (?, ?, ?)"
-);
-//using bcrypt
-app.post("/sign-up", (req, res, next) => {
-  try {
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-      if (err) {
-        return next(err);
-      }
-
-      try {
-        insertUserStmt.run(req.body.username, req.body.email, hashedPassword);
-        res.redirect("/");
-      } catch (err) {
-        return next(err);
-      }
-    });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-app.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/sign-up",
-  })
-);
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
---
+++ main/routes/authRoute.js
@@ -0,0 +1,50 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import db from "../../prisma/database.js";
+import passport from "../../passport/passportConfig.js";
+const router = express.Router();
+
+router.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+const insertUserStmt = db.prepare(
+  "INSERT INTO User (username,email, password) VALUES (?, ?, ?)"
+);
+
+//using bcrypt
+router.post("/sign-up", (req, res, next) => {
+  try {
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      }
+
+      try {
+        insertUserStmt.run(req.body.username, req.body.email, hashedPassword);
+        res.redirect("/");
+      } catch (err) {
+        return next(err);
+      }
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/sign-up",
+  })
+);
+
+router.post("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+export default router;
