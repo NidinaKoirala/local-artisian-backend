8c38501d3abc72908563ba6ffdfd860e03c2c80f
NidinaKoirala <108610633+NidinaKoirala@users.noreply.github.com>
2024-10-28 22:18:11 +0545

    Update app.js

--- main/app.js
+++ main/app.js
@@ -7,13 +7,11 @@ import authRoute from "./routes/authRoute.js";
 import adminRoute from "./routes/adminRoute.js";
 import itemRoute from "./routes/itemRoute.js";
 
-const PORT = 5174;
 const app = express();
 
-
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true, // Allows cookies and sessions to be sent with requests
+  origin: process.env.FRONTEND_URL || "https://nidinakoirala.github.io/Local-artisian-market-place",
+  credentials: true,
 }));
 
 app.set("view engine", "ejs");
@@ -23,7 +21,6 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 app.use("/auth", authRoute);
 app.use("/admin", adminRoute);
 app.use("/items", itemRoute);
@@ -31,4 +28,4 @@ app.use("/items", itemRoute);
 app.get("/", (req, res) => res.render("index", { user: req.user, req: req }));
 app.get("/books", authorize(['see_item', 'chat']), (req, res) => res.json({ name: 'harry potter' }));
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+export default app;
