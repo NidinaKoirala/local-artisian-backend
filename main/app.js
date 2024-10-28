38d35bce7976f3713485edde5388efc781b9528b
NidinaKoirala <108610633+NidinaKoirala@users.noreply.github.com>
2024-10-28 22:49:06 +0545

    Update app.js

--- main/app.js
+++ main/app.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Enable CORS for requests from your frontend origin
 app.use(cors({
-  origin: "http://localhost:5173", // Replace with your frontend's URL
+  origin: "https://nidinakoirala.github.io/Local-artisian-market-place", // Replace with your frontend's URL
   credentials: true, // Allows cookies and sessions to be sent with requests
 }));
 
