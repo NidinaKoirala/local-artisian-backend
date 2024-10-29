b8732e9e7839ca50337082d26550ed91c9ac308b
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-10-29 10:45:49 +0545

    get categories and products from category

--- main/routes/itemRoute.js
+++ main/routes/itemRoute.js
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const router = Router();
 
-router.get("/products", async (req, res)=> {
+router.get("/items", async (req, res)=> {
     try {
         const items = await getItems();
         res.json(items);
@@ -14,7 +14,37 @@ router.get("/products", async (req, res)=> {
     }
 });
 
-router.get("/products/:id", async (req, res)=> {
+router.get("/items/categories", async (req,res) =>{
+    try {
+        const distinctCategories = await prisma.item.findMany({
+            select: {
+                category: true, // Select only the category field
+            },
+            distinct: ['category'], // Ensure categories are distinct
+        });
+        const categories = distinctCategories.map(item => item.category);
+        res.json(categories);
+    } catch (error) {
+        res.status(500).json({error: "Failed to fetch categories"});
+    }
+})
+
+router.get("/items/categories/:category", async (req,res) => {
+    const category = req.params.category;
+    try {
+        const products = await prisma.item.findMany({
+            where: {
+                category: category, // Filter by the specified category
+            },
+        });
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({error: 'Failed to fetch the category items'})
+    }
+});
+
+//keep this last, as :id can conflict with stuff like "categories"
+router.get("/items/:id", async (req, res)=> {
     try {
         const id = Number(req.params.id);
         const item =  await prisma.item.findUnique({
@@ -23,7 +53,7 @@ router.get("/products/:id", async (req, res)=> {
         res.json(item);
     } catch (error) {
         console.log("Error fetching items", error);
-        res.status(500).json({ error: "Failed to fetch items" });
+        res.status(500).json({ error: "Failed to fetch item" });
     }
 });
 
