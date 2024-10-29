fa5aab0b83a984371fc91aedcc102bfae3768018
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-10-29 10:09:04 +0545

    get items by their id

--- main/routes/itemRoute.js
+++ main/routes/itemRoute.js
@@ -1,14 +1,26 @@
 import { Router } from "express";
 import getItems from "../../prisma/itemsData.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const router = Router();
 
-let items;
-
-console.log(items);
 router.get("/products", async (req, res)=> {
     try {
-        items = await getItems();
+        const items = await getItems();
         res.json(items);
+    } catch (error) {
+        console.log("Error fetching items", error);
+        res.status(500).json({ error: "Failed to fetch items" });
+    }
+});
+
+router.get("/products/:id", async (req, res)=> {
+    try {
+        const id = Number(req.params.id);
+        const item =  await prisma.item.findUnique({
+            where: { id },
+            })
+        res.json(item);
     } catch (error) {
         console.log("Error fetching items", error);
         res.status(500).json({ error: "Failed to fetch items" });
