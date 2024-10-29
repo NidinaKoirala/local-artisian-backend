5b065cc91a3423787d2f2001d692f370236b2a96
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-10-29 10:13:21 +0545

    login to see books. products in / now

--- main/app.js
+++ main/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/admin", adminRoute);
-app.use("/items", itemRoute);
+app.use("/", itemRoute);
 
 app.get("/", (req, res) => res.render("index", { user: req.user, req: req }));
 app.get("/books", authorize(['see_item', 'chat']), (req,res)=> res.json({name:'harry potter'}));
